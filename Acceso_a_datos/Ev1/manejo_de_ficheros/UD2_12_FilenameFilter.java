2ed2a4d01d12da967edfac39d5d49fe768d25d1a
DAM2Alu13 <DAM2Alu13@B01-PC05.IESGUARA.LOCAL>
2022-09-21 08:53:28 +0200

    Arreglo ejercicios 12

--- Acceso_a_datos/Ev1/manejo_de_ficheros/UD2_12_FilenameFilter.java
+++ Acceso_a_datos/Ev1/manejo_de_ficheros/UD2_12_FilenameFilter.java
@@ -9,30 +9,39 @@ import java.nio.file.attribute.FileTime;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 public class UD2_12_FilenameFilter implements FilenameFilter {
 
 	public boolean accept(File directorio, String ext) {
 		if (directorio.isDirectory() && directorio.exists()) {
 			File[] listaFicheros = directorio.listFiles();
+			boolean encontrado = false;
 			for (File file : listaFicheros) {
-				System.out.println("Nombre: " + file.getName());
-				System.out.println("Tamaño: " + file.length());
-				
-				// Fecha de creación de un archivo
-				BasicFileAttributes atributos_basicos = null;
-				try {
-					atributos_basicos = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
-				} catch (IOException e) {
-					e.printStackTrace();
+				if (file.getName().contains(ext)) {
+					encontrado = true;
+					System.out.println("Nombre: " + file.getName());
+					System.out.println("Tamaño: " + file.length());
+
+					// Fecha de creación de un archivo
+					BasicFileAttributes atributos_basicos = null;
+					try {
+						atributos_basicos = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
+					FileTime tiempo_crudo = atributos_basicos.creationTime();
+
+					SimpleDateFormat formato_dia = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+					String tiempo_formateado = formato_dia.format(new Date(tiempo_crudo.toMillis()));
+
+					System.out.println("Fecha de creación: " + tiempo_formateado + "\n");
 				}
-				
-				FileTime tiempo_crudo = atributos_basicos.creationTime();
-				
-				SimpleDateFormat formato_dia = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-				
-				String tiempo_formateado = formato_dia.format(new Date(tiempo_crudo.toMillis()));
-				
-				System.out.println("Fecha de creación: " + tiempo_formateado + "\n");
+			}
+			if (!encontrado) {
+				System.out.println("Archivos no encontrados con la extensión: " + ext);
 			}
 		} else {
 			System.out.println("El directorio o la extensión no es valido.");
