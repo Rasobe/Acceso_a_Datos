7fe49075b3ccdca51b70782743e12f800dd1c11d
Rasobe <rasoberdiel3@gmail.com>
2022-09-29 10:01:35 +0200

    ejercicio 11 hecho x2 tanto leer y escribir

---
+++ Acceso_a_datos/Ev1/manejo_de_ficheros/UD2_11_crear_archivo_v1.java
@@ -0,0 +1,44 @@
+package manejo_de_ficheros;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+
+import objetos.Pelicula;
+
+public class UD2_11_crear_archivo_v1 {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			Gson gson = new Gson();
+			
+			// Creamos las películas y las almacenamos en una lista.
+			
+			List<Pelicula> peliculas = new ArrayList<>();
+			peliculas.add(new Pelicula(1, "El señor de los Anillos: la Comunidad del Anillo", 2001,
+					"Ambientada en la tierra Media, cuenta la historia del Señor Oscuro Sauron, que es´ta buscando el Anillo Único, el cual ha acabado en poder de hobbit Frodo Bolsón (Elijah Wood)..."));
+			peliculas.add(new Pelicula(2, "El Señor de los Anillos: las dos torres", 2002,
+					"La trama de la película comienza tras la disolución de la Compañía del Anillo."));
+			peliculas.add(new Pelicula(3, "El Señor de los Anillos: el retorno del rey", 2003,
+					"Trata sobre la última parte del viaje que emprendieron los nueve compañeros (de los cuales quedan solamente ocho) para slavar la Tierra Media de la oscuridad impuesta por Sauron."));
+			
+			// Creamos un FileWriter para escribir el archivo JSON de la siguiente manera.
+			
+			FileWriter fw = new FileWriter("ficheros/Ev1/UD2/ej11.json");
+			gson.toJson(peliculas, fw);
+			fw.close();
+			System.out.println("Archivo creado correctamente!");
+			
+		} catch (JsonIOException | IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ Acceso_a_datos/Ev1/manejo_de_ficheros/UD2_11_leer_archivo_v2.java
@@ -0,0 +1,35 @@
+package manejo_de_ficheros;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class UD2_11_leer_archivo_v2 {
+
+	public static void main(String[] args) {
+
+		try {
+
+			String fichero = new String(Files.readAllBytes(Paths.get("ficheros/Ev1/UD2/ej11.json")));
+			// Parseamos el string para que no salga deprecated.
+			JsonArray peliculas = JsonParser.parseString(fichero).getAsJsonArray();
+
+			for (JsonElement jsonElement : peliculas) {
+				JsonObject pelicula = jsonElement.getAsJsonObject();
+				System.out.println("Id: " + pelicula.get("id").getAsInt() + "\nTitulo: " + pelicula.get("titulo")
+						+ "\nAnyo creación: " + pelicula.get("anyo").getAsInt() + "\nDescripción: "
+						+ pelicula.get("descripcion").getAsString() + "\n");
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
